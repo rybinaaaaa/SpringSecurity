1e8c1e86b9ace9fb94232e5efd49b883be9a1a72
rybinali <rybinaaaaa.a@gmail.com>
2023-10-20 18:02:58 +0200

    delete provider

--- src/main/java/rybina/SpringBootSecurity/config/SecurityConfig.java
+++ src/main/java/rybina/SpringBootSecurity/config/SecurityConfig.java
@@ -1,38 +1,18 @@
 package rybina.SpringBootSecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-//    private final AuthProviderImpl authProviderImpl;
-//
-//    @Autowired
-//    public SecurityConfig(AuthProviderImpl authProviderImpl) {
-//        this.authProviderImpl = authProviderImpl;
-//    }
-
-
-//    @Bean
-//    public AuthenticationManager authManager(HttpSecurity http) throws Exception {
-//        AuthenticationManagerBuilder authenticationManagerBuilder =
-//                http.getSharedObject(AuthenticationManagerBuilder.class);
-//        authenticationManagerBuilder.authenticationProvider(authProviderImpl);
-//        return authenticationManagerBuilder.build();
-//    }
-
-//    это нам не надо, провайдер автоматически встраивается, я в шоке!!
-
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http.authorizeRequests()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic(Customizer.withDefaults());
-//        return http.build();
-//    }
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
 
