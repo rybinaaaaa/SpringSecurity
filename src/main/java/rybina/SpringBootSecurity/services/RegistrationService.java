893203511eae7f81816ff93a89f380301fdd7704
rybinali <rybinaaaaa.a@gmail.com>
2023-10-22 21:34:27 +0200

    add encoding

--- src/main/java/rybina/SpringBootSecurity/services/RegistrationService.java
+++ src/main/java/rybina/SpringBootSecurity/services/RegistrationService.java
@@ -1,6 +1,7 @@
 package rybina.SpringBootSecurity.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import rybina.SpringBootSecurity.model.Person;
@@ -10,14 +11,21 @@ import rybina.SpringBootSecurity.repositories.PeopleRepository;
 public class RegistrationService {
 
     private final PeopleRepository peopleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepository peopleRepository) {
+    public RegistrationService(PeopleRepository peopleRepository, PasswordEncoder passwordEncoder) {
         this.peopleRepository = peopleRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person) {
+        String password = person.getPassword();
+        String encodedPass = passwordEncoder.encode(password);
+
+        person.setPassword(encodedPass);
+
         peopleRepository.save(person);
     }
 }
